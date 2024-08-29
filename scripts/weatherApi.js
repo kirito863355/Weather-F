e22436cd431c1b9896adc763cd968b7bfea89e15
Vitalii <vitalijmarchuk2508@gmail.com>
2024-08-29 21:01:27 +0200

    cst

--- scripts/weatherApi.js
+++ scripts/weatherApi.js
@@ -5,18 +5,54 @@ let btn = document.getElementById('btn');
 
 let city;
 
-let apiForecast = `https://api.weatherapi.com/v1/forecast.json?key=2d770a25df6c440f859180447242708&q=${city}&days=1&aqi=no&alerts=no`; // forecast
+const item_1 = document.querySelector('.item-1');
 
 async function getInfo(api){
     let response = await fetch(api);
     let result = await response.json();
-    
-    console.log(result);
+
+    console.log(result);    
+
+    item_1.innerHTML = `
+        <div>
+            <span>Now</span>
+            <img src="./icons/Image.svg" alt="">
+            <span style="font-weight: bold;">${result.current.temp_c}</span>
+        </div>
+        div>
+            <span>${}</span>
+            <img src="" alt="">
+            <span>${}</span>
+        </div>
+        <div>
+            <span>${}</span>
+            <img src="" alt="">
+            <span>${}</span>
+        </div>
+        <div>
+            <span>${}</span>
+            <img src="" alt="">
+            <span>${}</span>
+        </div>
+        <div>
+            <span>${}</span>
+            <img src="" alt="">
+            <span>${}</span>
+        </div>
+        <div>
+            <span>${}</span>
+            <img src="" alt="">
+            <span>${}</span>
+        </div>        
+        
+    `;
     
 }
 
+
 btn.onclick = () => {
     city = search.value;
-    let api = `https://api.weatherapi.com/v1/current.json?key=2d770a25df6c440f859180447242708&q=${city}&aqi=no`; // current
+    let api = `https://api.weatherapi.com/v1/forecast.json?key=2d770a25df6c440f859180447242708&q=${city}&days=6&aqi=no&alerts=no`; // forecast
+
     getInfo(api);
 }
